1725abaa7cdb99caec237bd209f369ff1c52e998
bryan.gonzalez96.bg@gmail.com <bryan.gonzalez96.bg@gmail.com>
2018-09-17 14:07:59 -0500

    first commit

--- src/Components/Recipes/Recipe/Recipe.js
+++ src/Components/Recipes/Recipe/Recipe.js
@@ -1,48 +1,64 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-import axios from 'axios';
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
 
-import './Recipe.css';
+import "./Recipe.css";
 
-
-
-const API_KEY = 'ef0d357a71c094e6330edb470f26de18';
-
+const API_KEY = "ef0d357a71c094e6330edb470f26de18";
 
 class Recipe extends Component {
-
   state = {
     activeRecipe: []
-  }
+  };
 
   componentDidMount = async () => {
-    const title = this.props.location.state.recipe
+    const title = this.props.location.state.recipe;
     // const req = await fetch(`https://cors-anywhere.herokuapp.com/http://food2fork.com/api/search?key=${API_KEY}&q=${title}`)
-    axios.get(`https://cors-anywhere.herokuapp.com/http://food2fork.com/api/search?key=${API_KEY}&q=${title}`)
-      .then(res => this.setState({ activeRecipe: res.data.recipes[0] }))
+    axios
+      .get(
+        `https://cors-anywhere.herokuapp.com/http://food2fork.com/api/search?key=${API_KEY}&q=${title}`
+      )
+      .then(res => this.setState({ activeRecipe: res.data.recipes[0] }));
     // console.log(this.props)
-  }
-
+  };
 
   render() {
     return (
       <div className="Recipe NoOverflow">
-        {
-          this.state.activeRecipe.length !== 0 &&
+        {this.state.activeRecipe.length !== 0 && (
           <div className="Recipe_Contain">
-            <img src={this.state.activeRecipe.image_url} alt={this.state.activeRecipe.title} />
+            <img
+              src={this.state.activeRecipe.image_url}
+              alt={this.state.activeRecipe.title}
+            />
             <h3 className="Title">{this.state.activeRecipe.title}</h3>
             <div className="Recipe_Container">
-              <h4> Publisher: <span className="Recipe_Publisher">{this.state.activeRecipe.publisher}</span></h4>
-              <h4> Website: <span className="Website" ><a href={this.state.activeRecipe.source_url} target="_blank" >{this.state.activeRecipe.source_url}</a></span></h4>
+              <h4>
+                {" "}
+                Publisher:{" "}
+                <span className="Recipe_Publisher">
+                  {this.state.activeRecipe.publisher}
+                </span>
+              </h4>
+              <h4>
+                {" "}
+                Website:{" "}
+                <span className="Website" style={{ fontWeight: 400 }}>
+                  <a href={this.state.activeRecipe.source_url} target="_blank">
+                    {this.state.activeRecipe.source_url}
+                  </a>
+                </span>
+              </h4>
             </div>
             <div className="Recipe_HomeBtn">
-              <h4><Link to="/">Home</Link></h4>
+              <h4>
+                <Link to="/">Home</Link>
+              </h4>
             </div>
           </div>
-        }
+        )}
       </div>
-    )
+    );
   }
 }
 
